84fc3ded71b62b181978f742cb4374fc9c624294
Tatiana Smith <tsmith167@cnm.edu>
2023-10-16 14:04:54 -0600

    added tables

--- sql/food-truck.sql
+++ sql/food-truck.sql
@@ -9,38 +9,38 @@ CREATE TABLE IF NOT EXISTS profile(
 )
 
 
--- create table if not exists truck(
---     truck_id UUID NOT NULL PRIMARY KEY,
---     truck_profile_id UUID NOT NULL,
---     truck_description VARCHAR(1025),
---     truck_food_category VARCHAR(64) NOT NULL,
---     truck_name VARCHAR(64) NOT NULL,
---     FOREIGN KEY (truck_profile_id) references profile (profile_id)
--- )
---
--- create table if not exists location(
---     location_id UUID NOT NULL PRIMARY KEY,
---     location_truck_id UUID NOT NULL,
---     location_is_active BOOLEAN NOT NULL,
---     location_address VARCHAR (64),
---     location_Lat DECIMAL (8,6),
---     location_Lng DECIMAL (9,6),
---     location_sunrise
---     location_sunset ,
---     FOREIGN KEY (location_truck_id) references truck(truck_id)
--- )
---
--- create table if not exists picture(
---     picture_id UUID NOT NULL PRIMARY KEY,
---     picture_truck_id UUID NOT NULL,
---     picture_url VARCHAR (125),
---     picture_type VARCHAR (32)
---    FOREIGN KEY (picture_truck_id) references truck(truck_id)
--- )
---
--- create table if not exists favorite(
---     favorite_truck_id UUID NOT NULL,
---     favorite_profile_id UUID NOT NULL,
---     FOREIGN KEY (favorite_truck_id) references truck(truck_id),
---     FOREIGN KEY (favorite_profile_id) references profile(profile_id)
---  )
+create table if not exists truck(
+    truck_id UUID NOT NULL PRIMARY KEY,
+    truck_profile_id UUID NOT NULL,
+    truck_description VARCHAR(1025),
+    truck_food_category VARCHAR(64) NOT NULL,
+    truck_name VARCHAR(64) NOT NULL,
+    FOREIGN KEY (truck_profile_id) references profile (profile_id)
+)
+
+create table if not exists location(
+    location_id UUID NOT NULL PRIMARY KEY,
+    location_truck_id UUID NOT NULL,
+    location_is_active BOOLEAN NOT NULL,
+    location_address VARCHAR (64),
+    location_Lat DECIMAL (8,6),
+    location_Lng DECIMAL (9,6),
+    location_sunrise
+    location_sunset ,
+    FOREIGN KEY (location_truck_id) references truck(truck_id)
+)
+
+create table if not exists picture(
+    picture_id UUID NOT NULL PRIMARY KEY,
+    picture_truck_id UUID NOT NULL,
+    picture_url VARCHAR (125),
+    picture_type VARCHAR (32)
+   FOREIGN KEY (picture_truck_id) references truck(truck_id)
+)
+
+create table if not exists favorite(
+    favorite_truck_id UUID NOT NULL,
+    favorite_profile_id UUID NOT NULL,
+    FOREIGN KEY (favorite_truck_id) references truck(truck_id),
+    FOREIGN KEY (favorite_profile_id) references profile(profile_id)
+ )
