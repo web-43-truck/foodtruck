6afea642e10e31b21aba5722b2b5e14d2b1530a0
Azucena Marroquin <atrevizo4@cnm.edu>
2023-10-18 14:09:44 -0600

    sign up route complete and validated, insomnia tested and passed, profile table is working

--- backend/src/apis/profile/profile.model.ts
+++ backend/src/apis/profile/profile.model.ts
@@ -6,9 +6,10 @@ import {PrivateProfileSchema} from "./profile.validator";
 export type PrivateProfile =  z.infer<typeof PrivateProfileSchema>;
 
 export async function insertProfile (profile : PrivateProfile): Promise<string> {
-    const {profileActivationToken, profileEmail, profileHash, profileIsTruckOwner, profileName } = profile
-    await sql`INSTERT INTO profile(profile_id, profile_activation_token,profile_email, profile_hash, profile_name, profile_is_truck_owner VALUES (gen_random_uuid() , ${profileActivationToken} , ${profileEmail}, ${profileHash} , ${profileName} , ${profileIsTruckOwner})`
-    return `Profile Successfully Created`
+
+    const {profileId, profileActivationToken, profileEmail, profileHash, profileName, profileIsTruckOwner} = profile
+    await sql`INSERT INTO profile (profile_id, profile_activation_token, profile_email, profile_hash, profile_name, profile_is_truck_owner) VALUES (gen_random_uuid(), ${profileActivationToken} , ${profileEmail}, ${profileHash}, ${profileName}, ${profileIsTruckOwner})`
+    return 'Profile Successfully Created'
 }
 
 
