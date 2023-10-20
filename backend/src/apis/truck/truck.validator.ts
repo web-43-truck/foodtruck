8de3192a4335da81a784fc3b072359edc7b01ebc
Azucena Marroquin <atrevizo4@cnm.edu>
2023-10-20 11:44:28 -0600

    completed truck validator

--- backend/src/apis/truck/truck.validator.ts
+++ backend/src/apis/truck/truck.validator.ts
@@ -13,14 +13,20 @@ export const truckSchema = z.object({
     })
         .uuid({message: 'please provide a valid truckProfileId'}),
 
+    truck_description: z.string({
+        required_error: 'truckDescription is a required field',
+        invalid_type_error: 'please provide a valid truckDescription'
+    })
+        .max(1025, {message: 'truck description is too long'})
+        .nullable(),
+
     truck_food_category: z.string({
         required_error: 'please provide a truckFoodCategory',
         invalid_type_error: 'please provide a valid foodTruckCategory'
-    }),
-
+    })
+        .max(64, {message: 'truckFoodCategoy is too long'}),
     truck_name: z.string()
         .trim()
-        .min(1, {message: 'please provide a valid name that is min 1 character in length'})
-        .max(32, {message: 'please provide a valid name that is max 32 characters in length'})
+        .min(1, {message: 'please provide a valid truck name that is min 1 character in length'})
+        .max(32, {message: 'please provide a valid truck name that is max 32 characters in length'})
 })
-
