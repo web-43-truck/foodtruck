45aeaf5edf295156cace0acd1758e3508b907be8
444lyssa <acave4@cnm.edu>
2023-10-21 15:30:04 -0600

    added code for picture controller and picture route, questions for instructors to be determined

--- backend/src/apis/picture/picture.route.ts
+++ backend/src/apis/picture/picture.route.ts
@@ -1,1 +1,12 @@
-GIT 
+import { Router } from 'express'
+import { pictureUploader } from '../../utils/controllers/multer.controller'
+import { pictureUploadController } from './picture.contoller'
+
+const basePath = '/apis/picture'
+
+const router = Router()
+
+router.route('/')
+    .post(pictureUploader, pictureUploadController)
+
+export const imageRoute = { basePath, router }
