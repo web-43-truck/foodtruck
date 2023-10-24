d70dff21e7d908bcf2c030993ca18e042ef98d28
Azucena Marroquin <atrevizo4@cnm.edu>
2023-10-23 18:25:29 -0600

    truck api merged with development branch and truck route added to app.ts

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -8,6 +8,7 @@ import RedisStore from 'connect-redis'
 import { signUpRoute } from './apis/sign-up/sign-up.route'
 import { signInRoute} from './apis/sign-in/sign-in.route'
 import {profileRoute} from './apis/profile/profile.route'
+import {truckRoute} from "./apis/truck/truck.route";
 
 // The following class creates the app and instantiates the server
 export class App {
@@ -54,6 +55,7 @@ export class App {
         this.app.use(signUpRoute.basePath, signUpRoute.router)
         this.app.use(signInRoute.basePath, signInRoute.router)
         this.app.use(profileRoute.basePath, profileRoute.router)
+        this.app.use(truckRoute.basePath, truckRoute.router)
     }
 
     // starts the server and tells the terminal to post a message that the server is running and on what port
