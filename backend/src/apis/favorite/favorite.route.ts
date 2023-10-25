df418132dafb3c867c27284887255bb15f864a66
Azucena Marroquin <atrevizo4@cnm.edu>
2023-10-24 19:08:53 -0600

    finished favorite route

--- backend/src/apis/favorite/favorite.route.ts
+++ backend/src/apis/favorite/favorite.route.ts
@@ -1,1 +1,29 @@
+import { Router } from 'express'
+import {isLoggedInController} from "../../utils/controllers/isLoggedIn.controller"
+import {
+    deleteFavoriteController, getFavoritesByFavoriteProfileIdController,
+    getFavoritesByFavoriteTruckIdController,
+    postFavoriteController,
+    toggleFavoriteController
+} from "./favorite.controller"
 
+import basePath = '/apis/favorite'
+
+
+const router = Router()
+
+router.route('/')
+    .post(isLoggedInController, postFavoriteController)
+
+router.route('/toggle')
+    .post(isLoggedInController, toggleFavoriteController)
+
+router.route('/favoriteTruckId/:favoriteTruckId')
+    .get(getFavoritesByFavoriteTruckIdController)
+    .delete(isLoggedInController, deleteFavoriteController)
+
+router.route('/favoriteProfileId/:favoriteProfileId')
+    .get(getFavoritesByFavoriteProfileIdController)
+
+
+export const favoriteRoute = {basePath, router}
