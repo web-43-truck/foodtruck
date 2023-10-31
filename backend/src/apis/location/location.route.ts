c89c8ceeabdfe06ef22f7d4d6530396066d2f857
Tatiana Smith <tsmith167@cnm.edu>
2023-10-31 14:32:17 -0600

    l r

--- backend/src/apis/location/location.route.ts
+++ backend/src/apis/location/location.route.ts
@@ -3,19 +3,28 @@ import {
 
 
 } from "./location.controller"
+import {
+    selectLocationByLocationAddress,
+    selectLocationByLocationId, selectLocationByLocationSunrise, selectLocationByLocationSunset,
+    selectLocationByLocationTruckId
+} from "./location.model";
 const basePath = '/apis/location'
 
 const router = Router()
 
 
 
-
-
+router.route('/locationAddress')
+    .get(selectLocationByLocationAddress)
+router.route('/locationSunrise')
+    .get(selectLocationByLocationSunrise)
+router.route('/locationSunset')
+    .get(selectLocationByLocationSunset)
 router.route('/locationId')
- //   .get(getLocationByLocationId)
+    .get(selectLocationByLocationId)
 
 router.route('/locationTruckId')
-    .get(getLocationByLocationTruckId)
+    .get(selectLocationByLocationTruckId)
 
 
 
