ef7f62cd8c9fa3b06da22ce45efd94ac20caaea1
Azucena Marroquin <atrevizo4@cnm.edu>
2023-11-02 13:36:29 -0600

    the majority of the search is finished, added a listitem component

--- frontend/src/components/SearchItem.tsx
+++ frontend/src/components/SearchItem.tsx
@@ -2,8 +2,8 @@ export function SearchItem(searchItemDescriptors: SearchItemDescriptors){
     const {truckName, truckDescription} = searchItemDescriptors
     return (
         <>
-            <section className="py-6 px-8">
-                <h2 className="text-xl">{truckName}</h2>
+            <section className="py-6 px-14">
+                <h2 className="text-xl underline">{truckName}</h2>
                 <p className="text-lg">{truckDescription}</p>
             </section>
         </>
