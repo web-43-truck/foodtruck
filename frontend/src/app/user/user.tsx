fc874a9193927630ae3e32c68bc58b9a715af090
Tatiana Smith <tsmith167@cnm.edu>
2023-11-02 13:58:28 -0600

    grid, checkbox

---
+++ frontend/src/app/user/user.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+
+import Grid from "../../components/grid"
+import React from 'React'
+import PropTypes from 'prop-types'
+
+export default function Home() {
+    return (
+        <>
+            <H1 className"text-center text-8xl mt-14">Truck List</H1>
+        <Grid/>
+
+
+
+        </>
+    )
+}
--- frontend/src/components/grid.tsx
+++ frontend/src/components/grid.tsx
@@ -1,10 +1,11 @@
 import CheckBox from "./checkbox";
+import Picture from "@/components/Picture";
+import SearchItem from "@/components/SearchItem";
 
 export default function Grid() {
     return (
 
-        <div
-            className="grid grid-cols-3 gap-3  items-center group md:lg:xl:border-r md:lg:xl:border-b mt-4 text-gray-950 mb-2 py-2 px-2 h-60 text-sm">
+        <div className="grid grid-cols-3 gap-3  items-center group md:lg:xl:border-r md:lg:xl:border-b mt-4 text-gray-950 mb-2 py-2 px-2 h-60 text-sm">
 
             <SearchItem truckName={"Joes Tacos"} truckDescription={"Cookie croissant jelly cake carrot cake cotton candy caramels cotton candy. Icing oat souffle. Gingerbread oat cake sweet roll powder jelly-o."}/>
 
