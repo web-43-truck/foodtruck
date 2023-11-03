200c35b5a20c788d2310b9506f2aefe5d93f53f8
Tatiana Smith <tsmith167@cnm.edu>
2023-11-02 20:56:02 -0600

    navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -32,41 +32,9 @@ function NavBarTitle (){
     )
 }
 
-<<<<<<< HEAD
-function NavLinks(props: {children: any}){
-    return(
-        <div className="basis-11/12 ">
-                <div className="navbar-start md:hidden ">
-                    <div className="dropdown ">
-                        <label tabIndex={0} className="btn btn-ghost btn-square">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 stroke-current" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className=" menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52">
-                            <li><a>home</a></li>
-                            <li><a>log-in</a></li>
-                            <li><a>sign-up</a>href={"SignUp"}</li>
-                            <li><a>favorite</a></li>
-                        </ul>
-                    </div>
-                </div>
-            <ul className="menu menu-horizontal flex-none ml-28 md:ml-2">
-                {props.children}
-            </ul>
-        </div>
-    )
-}
 
-type NavLinkProps = {
-    linkText: string,
-    href: string
-}
-function NavLink(props: NavLinkProps){
-    const {linkText, href} = props
-    return(
-        <li className="justify-end ml-2"><a href={href}>{linkText}</a></li>
-    )
-}
-=======
+
+
 // function NavLinks(props: {children: any}){
 //     return(
 //         <div className="basis-11/12 ">
@@ -94,4 +62,4 @@ function NavLink(props: NavLinkProps){
 //         <li className="justify-end ml-2"><a href={href}>{linkText}</a></li>
 //     )
 // }
->>>>>>> ca8ef189af185f11c8fbbc3e68a1bef864be6b51
+
