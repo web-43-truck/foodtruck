6a0878ccd74244d0bece8dba9d445d17362e1f19
444lyssa <acave4@cnm.edu>
2023-11-03 13:20:10 -0600

    commiting searchbar code

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,19 @@
 import {NavBar} from "@/components/NavBar";
 import 'mapbox-gl/dist/mapbox-gl.css'
+import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
     return (
         <>
-        <NavBar/>
+            <NavBar/>
+            <SearchBar/>
         </>
 
 
     )
 }
+
+
+
+
+
--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,9 +1,31 @@
-import React from "react";
-import ( )
+import React, {useState} from "react";
+
 export function SearchBar({ onSearch }) {
-    const [query, setQuery] = useState('');
+    const [query, setQuery] = useState('')
+
+    const handleInputChange = (e : any) => {
+        const newQuery = e.target.value
+        setQuery(newQuery)
+        onSearch(newQuery)
+    };
+
+
     return (
-        <>
-        </>
+       <>
+         <div className="bg-base-300">
+             <input
+                 type="text"
+                 placeholder="Search"
+                 value={query}
+                 onChange={handleInputChange}
+             />
+         </div>
+       </>
     )
+}
+
+export default SearchBar
+
+function onSearch(newQuery: any) {
+    throw new Error("Function not implemented.");
 }
