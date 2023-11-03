2306620e1458b0c082e8fab2871f8a90b1c88c64
Azucena Marroquin <atrevizo4@cnm.edu>
2023-11-02 19:09:32 -0600

    updated navbar to be updated as needed

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"m0F8SEIy+N6qctUH6NTPfE3uttLpvWNiyxndWdLxAA0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"k/yluWkCswENdiAALj+AuWST85ltcMxZwe/gZtp+ifM=\"\n}"
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,20 @@
 import {NavBar} from "@/components/NavBar";
 import 'mapbox-gl/dist/mapbox-gl.css'
 
+
 export default function Home() {
+    const links = [
+        {linkName: 'Sign-in', href: '/'},
+        {linkName: 'Sign-up', href: '/'},
+        {linkName: 'Favorites', href: '/'},
+    ]
+
     return (
         <>
-        <NavBar/>
+        <section>
+            <NavBar links={links}/>
+        </section>
+
         </>
 
     )
