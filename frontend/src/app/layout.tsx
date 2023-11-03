cb8b00098ef5457c4748339eca58dffcdcf532ef
Azucena Marroquin <atrevizo4@cnm.edu>
2023-11-03 08:46:18 -0600

    playing around with nav bar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout(props : RootLayoutProps) {
     return (
         <html data-theme="light" lang="en">
         <body>{children}</body>
+
         </html>
     )
 }
