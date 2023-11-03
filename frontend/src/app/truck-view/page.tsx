6688feaa0630248ebaf6b6e24c643acf3989e6a7
Tatiana Smith <tsmith167@cnm.edu>
2023-11-03 08:37:46 -0600

    sign up

--- frontend/src/app/truck-view/page.tsx
+++ frontend/src/app/truck-view/page.tsx
@@ -5,7 +5,7 @@ import {TruckView} from "./TruckView";
 export default function TruckPage(){
     return(
         <>
-            <NavBar/>
+
             <TruckView key={process.env["MAPBOX_ACCESS_TOKEN"]}/>
         </>
     )
