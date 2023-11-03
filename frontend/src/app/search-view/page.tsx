0116d29bb79d1d058a144597cb7c8497c48ddd2f
Azucena Marroquin <atrevizo4@cnm.edu>
2023-11-03 09:11:57 -0600

    cleaned up the code and created Footer

--- frontend/src/app/search-view/page.tsx
+++ frontend/src/app/search-view/page.tsx
@@ -4,9 +4,16 @@ import {Picture} from "@/components/Picture";
 import {SearchView} from "@/app/search-view/SearchView";
 
 export default function SearchPage(){
+    const links = [
+        {linkName: 'Sign-in', href: '/'},
+        {linkName: 'Sign-up', href: '/'},
+        {linkName: 'Favorites', href: '/'}
+    ]
     return (
         <>
-            <NavBar/>
+            <section>
+                <NavBar links={links}/>
+            </section>
             <SearchView/>
         </>
     )
