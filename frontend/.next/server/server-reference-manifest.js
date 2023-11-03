0d40a21a029e9393c7afd98753882d1916557f24
Azucena Marroquin <atrevizo4@cnm.edu>
2023-11-03 13:47:45 -0600

    update

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+0nk+8+qPYVDu6eJMZAFW3UeBrtOPzxkj+Vt+DTdXdI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7DmmQXV/WGtz8ZtXoeT8qqi5yR4SCYSXR0tLOCFu65o=\"\n}"
