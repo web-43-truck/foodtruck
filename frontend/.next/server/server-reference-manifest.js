50e256aed22ec9a8a1ee34ac45383e562ad6198d
Tatiana Smith <tsmith167@cnm.edu>
2023-11-03 09:11:08 -0600

    .

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nRHl/CZ35924Ot8JST0ZuvIi9TAqaRSUAsijZB/FcUI=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"k/yluWkCswENdiAALj+AuWST85ltcMxZwe/gZtp+ifM=\"\n}"
->>>>>>> ca8ef189af185f11c8fbbc3e68a1bef864be6b51
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zemJluByGHV9w8g0CLTzXAHChRUkx0cwdQyalYa8gbg=\"\n}"
