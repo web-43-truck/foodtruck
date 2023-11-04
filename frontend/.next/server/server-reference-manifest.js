f70d737881921886f9a47448c3b64778913e9896
Tatiana Smith <tsmith167@cnm.edu>
2023-11-04 11:19:32 -0600

    removed location api

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7DmmQXV/WGtz8ZtXoeT8qqi5yR4SCYSXR0tLOCFu65o=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"C3a7rsnUXyx0eIbg1fzebrmVstzchwYlgXxtbahR+IM=\"\n}"
