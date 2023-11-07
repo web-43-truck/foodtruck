27328719580ce1d9c1b3426b7ce2ce077086e2fc
Azucena Marroquin <atrevizo4@cnm.edu>
2023-11-07 14:30:08 -0700

    issues with gitignore .next

--- frontend/src/app/truck/TruckView.tsx
+++ frontend/src/app/truck/TruckView.tsx
@@ -8,13 +8,17 @@ import {string} from "zod";
 import {Simulate} from "react-dom/test-utils";
 import error = Simulate.error;
 import {LocationSchema} from "@/utils/models/Location";
+import {SearchItem} from "@/components/SearchItem";
 
 
-export function TruckView() {
-
+export async function TruckView() {
+    const trucks = await getData()
+    console.log(trucks)
     return (
         <>
-            <h1 className="text-center text-8xl mt-14">Joe's Tacos</h1>
+            <h1 className="text-center text-8xl mt-14">
+                {trucks.length > 0 ? trucks[0].truckName : "No Truck Name"}
+            </h1>
             <section className="container mx-auto">
                 <div className="flex md:justify-evenly rounded-xl">
                     <Picture text={"https://placekitten.com/400/400"} alt={"placeholder"}/>
@@ -28,7 +32,14 @@ export function TruckView() {
                         <h2 >Open: 6pm</h2>
                         <h2 >Close: 10pm</h2>
                     </div>
-                    <p className="flex justify-center text-3xl mt-8 px-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem harum ipsa neque nobis reprehenderit totam voluptatibus! Alias, aut autem eum explicabo illo illum maxime molestias mollitia provident quasi sit vel!</p>
+                    <p className="flex justify-center text-3xl mt-8 px-20">
+                        {trucks.map((truck) => (
+                            <SearchItem
+                                key={truck.truckId}
+                                truckDescription = {truck.truckDescription}
+                            />
+                        ))}
+                    </p>
                     <div className="text-center text-2xl mt-8">
                         <h3 className="underline text-3xl">Address:</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
@@ -39,30 +50,19 @@ export function TruckView() {
     )
 }
 
-// async function getData(): Promise<{Truck[], locations:{[index:string]: Location}} > {
-//     console.log(process.env.REST_API_URL)
-//     const response = await fetch(`${process.env.REST_API_URL}/apis/truck`, {next: {revalidate: 0}});
-//     if (response.status === 200 || response.status === 304) {
-//         const result = await response.json();
-//         console.log(result)
-//         const trucks = TruckSchema.array().parse(result?.data);
-//         return  trucks ;
-//     } else {
-//         throw new Error("Retrieving data failed");
-//     }
-//
-//     const locations: {[index:string]: Location} = {}
-//     for (let truck of trucks) {
-//         const result = await fetch(`${process.env.REST_API_URL}/apis/location/${truck.locationTruckId}`)
-//             .then(response => {console.log(response.status)
-//             if (response.status === 200 || response.status === 304) {
-//                 return response.json()
-//             }
-//             throw new Error("Retrieving data failed")
-//             }).catch(error => {console.error(error)})
-//         const location = LocationSchema.parse(result?.data)
-//         locations[truck.locationTruckId] = location
-//     }
-// }
+async function getData(): Promise<Truck[] > {
+    console.log(process.env.REST_API_URL)
+    const response = await fetch(`${process.env.REST_API_URL}/apis/truck`, {next: {revalidate: 0}});
+    if (response.status === 200 || response.status === 304) {
+        const result = await response.json();
+        console.log(result)
+        const trucks = TruckSchema.array().parse(result?.data);
+        return  trucks ;
+    } else {
+        throw new Error("Retrieving data failed");
+    }
+
+
+}
 
 
