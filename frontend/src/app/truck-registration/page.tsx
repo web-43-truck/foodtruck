fceb79821f2b3cfe1a6fad41c9cd64bd02e7a1c9
JP Melech Solomon <jmelechsolomon@cnm.edu>
2023-11-07 14:26:14 -0700

    crying because

--- frontend/src/app/truck-registration/page.tsx
+++ frontend/src/app/truck-registration/page.tsx
@@ -18,7 +18,7 @@ export default function TruckRegistrationPage(){
                 <NavBar links={links}/>
             </section>
             <TruckRegistration/>
-            <TruckMap/>
+
             <Footer/>
         </>
     )
