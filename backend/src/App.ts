4dc670174342d491943732067d5595d5503296c4
Tatiana Smith <tsmith167@cnm.edu>
2023-11-08 12:01:29 -0700

    testing location

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -11,8 +11,10 @@ import { signUpRoute } from './apis/sign-up/sign-up.route'
 import { signInRoute } from './apis/sign-in/sign-in.route'
 import { profileRoute } from './apis/profile/profile.route'
 import { truckRoute } from './apis/truck/truck.route'
-import { favoriteRoute }  from "./apis/favorite/favorite.route";
-import { pictureRoute } from "./apis/picture/picture.route";
+import { favoriteRoute }  from "./apis/favorite/favorite.route"
+import { pictureRoute } from "./apis/picture/picture.route"
+import { locationRoute } from "./apis/location/location.route"
+
 
 
 // The following class creates the app and instantiates the server
@@ -64,6 +66,7 @@ export class App {
         this.app.use(truckRoute.basePath, truckRoute.router)
         this.app.use(favoriteRoute.basePath, favoriteRoute.router)
         this.app.use(pictureRoute.basePath, pictureRoute.router)
+        this.app.use(locationRoute.basePath, locationRoute.router)
     }
 
     // starts the server and tells the terminal to post a message that the server is running and on what port
