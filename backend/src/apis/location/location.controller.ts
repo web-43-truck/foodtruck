fc96eed92a30a03462156c80533dcbbb9a3cd2e0
Tatiana Smith <tsmith167@cnm.edu>
2023-11-08 15:48:05 -0700

    new branch favorite

--- backend/src/apis/location/location.controller.ts
+++ backend/src/apis/location/location.controller.ts
@@ -253,6 +253,8 @@ export async function getLocationByLocationSunrise(request: Request, response: R
 
 
 
+
+
 // async function getLocationByLocationLat() {
 //     new Promise((resolve, reject) => {
 //         if (navigator.geolocation) {
