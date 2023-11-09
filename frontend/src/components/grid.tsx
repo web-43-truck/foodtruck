e73116a384a43bbe8eb3c48a9bbeea506169ee24
Tatiana Smith <tsmith167@cnm.edu>
2023-11-09 08:59:06 -0700

    fixing grid

--- frontend/src/components/grid.tsx
+++ frontend/src/components/grid.tsx
@@ -10,7 +10,7 @@ export function Grid() {
 
         <div className="grid grid-cols-3 gap-3  items-center group md:lg:xl:border-r md:lg:xl:border-b mt-4 text-gray-950 mb-2 py-2 px-2 h-60 text-sm">
 
-            <SearchItem key={truck.truckId} truck={truck}/>
+            <SearchItem key={truck.truckName} truck={truckName}/>
 
             <Picture text={"https://placekitten.com/400/400"} alt={"placeholder"}/>
 
