74cbdc470dffcb20417a1a447daa2ba099ace632
Azucena Marroquin <atrevizo4@cnm.edu>
2023-11-12 19:35:16 -0700

    again .env.development problem

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8a754262af540a35"; }
+/******/ 		__webpack_require__.h = function() { return "04c633cc2d73b65b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
