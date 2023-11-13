ec9d7b9ee8e220f032d4224b2a9e83eeb5a38389
Azucena Marroquin <atrevizo4@cnm.edu>
2023-11-13 09:08:36 -0700

    trying to pull from development

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "35391f94ea61dfe0"; }
+/******/ 		__webpack_require__.h = function() { return "7b5b7a5d6547ba44"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
