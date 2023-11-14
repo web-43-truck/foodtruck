c824797bbc29e4ca9990cc3a528d655d14b3c4e6
444lyssa <acave4@cnm.edu>
2023-11-13 17:21:03 -0700

    tailwind config

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -163,7 +163,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/chunks/" + chunkId + ".js";
+/******/ 			return undefined;
 /******/ 		};
 /******/ 	}();
 /******/ 	
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c4175acda64db866"; }
+/******/ 		__webpack_require__.h = function() { return "9731e79b622eff44"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
